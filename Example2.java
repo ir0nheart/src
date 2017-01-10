0b03638d05c65f3c9ea2a634905b0314e8992b23
ir0nheart <alpaydemiryurek@Alpays-MacBook-Pro.local>
2017-01-09 20:41:55 -0500

    3 examples

---
+++ Example2.java
@@ -0,0 +1,13 @@
+public class Example2{
+
+	public static void main(String args[]){
+		int var;
+		double x;
+
+		var = 10;
+		x=10.0;
+
+	System.out.println("Original value of var:" + var);
+	System.out.println("Original value of x:" + x);
+	}
+}
